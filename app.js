86ecc67b630ee88ae39f07d2ea1c95bbfbb200ae
Drake <zudrake.yh@gmail.com>
2020-06-10 02:33:45 -0700

    basic Authentication

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const connect = mongoose.connect(url, {
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
-
 var app = express();
 
 // view engine setup
@@ -33,7 +32,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+    return next(); // authorized
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
